ff0e8e6049bc122f2a2c58f9cadc5a6eb3689742
Juan Jose Arcila <arcilaj2953@gmail.com>
2026-06-11 16:42:32 -0500

    Agrega validacion al modulo de contabilidad

--- js/contabilidad.js
+++ js/contabilidad.js
@@ -4,4 +4,34 @@ let ingresos = document.getElementById("ingresos").value;
 let egresos = document.getElementById("egresos").value;
 let concepto = document.getElementById("concepto").value;
 let valor = document.getElementById("valor").value;
-get_usuario_information
+let usuario = JSON.parse(localStorage.getItem("usuario"));
+
+function ValidarDatos() {
+    ventas = document.getElementById("ventas").value;
+    fecha = document.getElementById("fecha").value;
+    ingresos = document.getElementById("ingresos").value;
+    egresos = document.getElementById("egresos").value;
+    concepto = document.getElementById("concepto").value;
+    valor = document.getElementById("valor").value;
+
+    if (ventas === "" || fecha === "" || ingresos === "" || egresos === "" || concepto === "" || valor === "") {
+        console.log("los datos son incorrectos");
+    } else {
+        if (isNaN(ventas)) {
+            console.log("los datos son incorrectos");
+        }
+        if (isNaN(ingresos)) {
+            console.log("los datos son incorrectos");
+        }
+        if (isNaN(egresos)) {
+            console.log("los datos son incorrectos");
+        }
+        if (/\d/.test(concepto)) {
+            console.log("los datos son incorrectos");
+        }
+        if (isNaN(valor)) {
+            console.log("los datos son incorrectos");
+        }
+    }
+}
+
