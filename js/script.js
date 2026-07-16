ddedfd7bccbea8891e4640ae496f087ae08269d6
arcilaj2953-ctrl <arcilaj2953@gmail.com>
2026-07-16 17:31:38 -0500

    Delete js directory

--- js/script.js
+++
@@ -1,7 +0,0 @@
-function ValidarDatos() {
-    Swal.fire({
-        title: "Error de Validación",
-        text: "Los datos ingresados son incorrectos. Por favor, revíselos.",
-        icon: "warning"
-    });
-}
